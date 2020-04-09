d62b3199808dd346aed3d52bd19078132b7901d1
Kaliades <Szym.Kulinski@gmail.com>
2020-04-09 14:43:07 +0200

    Created two concepts of list items, added necessary dependencies for ui

---
+++ app/src/main/java/com/simon_kulinski/catfacts/ui/cat_fact_details/CatFactDetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.simon_kulinski.catfacts.ui.cat_fact_details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.simon_kulinski.catfacts.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CatFactDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cat_fact_details, container, false)
+    }
+
+}
